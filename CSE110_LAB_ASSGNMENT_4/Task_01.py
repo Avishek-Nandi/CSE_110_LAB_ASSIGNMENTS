06c0bd42a1a8b73035c6f36a4c5958ab60e1d534
Avishek Nandi Barna <103306246+Avishek-Nandi@users.noreply.github.com>
2022-04-26 17:31:37 +0600

    del

--- CSE110_LAB_ASSGNMENT_4/Task_01.py
+++
@@ -1,7 +0,0 @@
-# Solution to task 1
-
-list_of_numbers = []
-for i in range(1, 6):
-    number = int(input("Sir, please enter you desired number: "))
-    list_of_numbers.append(number)
-    print(list_of_numbers)
